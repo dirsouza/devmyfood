0510526709fe384e88984cf63d8f9c874fe3c1cf
dsouza <sys23@grupopmz.com.br>
2019-10-22 13:34:19 -0400

    first commit

---
+++ api/src/server.ts
@@ -0,0 +1,33 @@
+import { resolve } from "path";
+import { GraphQLServer } from "graphql-yoga";
+
+const USERS = [
+  { id: 1, name: "Tony Stark", email: "tony@stark.com" },
+  { id: 2, name: "Spider Man", email: "spider@man.com" }
+];
+
+const typeDefs = resolve(__dirname, "schema.graphql");
+const resolvers = {
+  Query: {
+    users: async () => USERS
+  },
+  Mutation: {
+    createUser: async (parent, { data }, ctx, info) => {
+      const user = {
+        id: USERS.length + 1,
+        ...data
+      };
+
+      USERS.push(user);
+
+      return user;
+    }
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+});
+
+export default server;
