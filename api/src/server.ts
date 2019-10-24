581f4c402c7ff4bb25def21ccd532c8de13b4881
Diogo Souza <diogo.souza@msn.com>
2019-10-24 08:18:30 -0400

    Configurado o Prettier

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,33 +1,33 @@
-import { resolve } from "path";
-import { GraphQLServer } from "graphql-yoga";
+import { resolve } from 'path'
+import { GraphQLServer } from 'graphql-yoga'
 
 const USERS = [
-  { id: 1, name: "Tony Stark", email: "tony@stark.com" },
-  { id: 2, name: "Spider Man", email: "spider@man.com" }
-];
+  { id: 1, name: 'Tony Stark', email: 'tony@stark.com' },
+  { id: 2, name: 'Spider Man', email: 'spider@man.com' },
+]
 
-const typeDefs = resolve(__dirname, "schema.graphql");
+const typeDefs = resolve(__dirname, 'schema.graphql')
 const resolvers = {
   Query: {
-    users: (): typeof USERS => USERS
+    users: (): typeof USERS => USERS,
   },
   Mutation: {
     createUser: async (parent, { data }, ctx, info) => {
       const user = {
         id: USERS.length + 1,
-        ...data
-      };
+        ...data,
+      }
 
-      USERS.push(user);
+      USERS.push(user)
 
-      return user;
-    }
-  }
-};
+      return user
+    },
+  },
+}
 
 const server = new GraphQLServer({
   typeDefs,
-  resolvers
-});
+  resolvers,
+})
 
-export default server;
+export default server
